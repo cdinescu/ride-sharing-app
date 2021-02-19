f00626a3ccdaac220fcecc32a5fc9d44579e13ee
Cristina Dinescu <crina91@gmail.com>
2021-02-19 21:39:04 +0200

    Ride Sharing Planning Service: Initial commit
    Signed-off-by: Cristina Dinescu <crina91@gmail.com>

---
+++ ride-planner-service/src/main/java/ride/sharing/app/rideplannerservice/RidePlannerServiceApplication.java
@@ -0,0 +1,13 @@
+package ride.sharing.app.rideplannerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidePlannerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidePlannerServiceApplication.class, args);
+	}
+
+}
---
+++ ride-planner-service/src/test/java/ride/sharing/app/rideplannerservice/RidePlannerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ride.sharing.app.rideplannerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidePlannerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
