9b6c75042629adeeb53d24633c30791463e65645
Cristina Dinescu <crina91@gmail.com>
2021-03-07 02:20:41 +0200

    Integrated payment-service
    Signed-off-by: Cristina Dinescu <crina91@gmail.com>

--- payment-service/src/main/java/com/ridesharing/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/ridesharing/payment/PaymentServiceApplication.java
@@ -3,6 +3,7 @@ package com.ridesharing.payment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
@@ -10,6 +11,7 @@ import reactor.core.scheduler.Schedulers;
 import java.util.concurrent.Executors;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
