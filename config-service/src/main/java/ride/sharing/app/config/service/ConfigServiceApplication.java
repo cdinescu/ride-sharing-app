289db1f8d7b4d08e3f0598905a466f6484192582
Cristina Dinescu <crina91@gmail.com>
2021-03-11 00:28:12 +0200

    Revert "Fixed codacy warnings"
    This reverts commit ccf854b4ce87e49760eb122f8be00ac36e2f1f7e.

--- config-service/src/main/java/ride/sharing/app/config/service/ConfigServiceApplication.java
+++ config-service/src/main/java/ride/sharing/app/config/service/ConfigServiceApplication.java
@@ -2,6 +2,7 @@ package ride.sharing.app.config.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
