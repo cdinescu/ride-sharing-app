772ecce9ecdaa282a658e54499e78ac63ade0aa6
Cristina Dinescu <crina91@gmail.com>
2021-03-11 18:23:20 +0200

    Testcontainer: dockerize the postgres database
    Signed-off-by: Cristina Dinescu <crina91@gmail.com>

---
+++ ride-planner-service/src/test/java/ride/sharing/app/rideplannerservice/dao/service/RidePlanningServiceIT.java
@@ -0,0 +1,8 @@
+package ride.sharing.app.rideplannerservice.dao.service;
+
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+class RidePlanningServiceIT extends  RidePlanningServiceTest {
+
+}
