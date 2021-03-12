8333142986ca08d5901f22d2c82b8d5c59bc1ed0
Cristina Dinescu <crina91@gmail.com>
2021-03-12 22:11:59 +0200

    log message
    Signed-off-by: Cristina Dinescu <crina91@gmail.com>

--- ride-planner-service/src/test/java/ride/sharing/app/rideplannerservice/kafka/KafkaConsumer.java
+++ ride-planner-service/src/test/java/ride/sharing/app/rideplannerservice/kafka/KafkaConsumer.java
@@ -13,6 +13,10 @@ public class KafkaConsumer {
     private CountDownLatch latch = new CountDownLatch(1);
     private String payload = null;
 
+    public KafkaConsumer() {
+        log.info("Boo! KafkaConsumer");
+    }
+
     @KafkaListener(topics = "ride-planning-topic", groupId = "group_id")
     public void receive(ConsumerRecord<?, ?> consumerRecord) {
         log.info("received payload='{}'", consumerRecord.toString());
